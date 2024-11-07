99d34d3a6f8766262ee8426877d57457f3e10d76
Hariyanto Lukman <hariyantosubang@gmail.com>
2024-11-07 20:13:20 +0700

    modified lifecycle manager

--- lib/initial/my_app_layout.dart
+++ lib/initial/my_app_layout.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:goes_hunt/core/lifecycle/lifecycle_manager.dart';
 
 class MyAppLayout extends StatefulWidget {
   const MyAppLayout({super.key});
@@ -10,6 +11,6 @@ class MyAppLayout extends StatefulWidget {
 class _MyAppLayoutState extends State<MyAppLayout> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return LifecycleManager(child: MaterialApp());
   }
 }
